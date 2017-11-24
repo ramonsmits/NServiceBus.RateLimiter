d7305c264b6dbf2d90ee67a4f95692859774bff1
Ramon Smits <ramon.smits@gmail.com>
2017-11-24 10:48:00 +0100

    Added logging of configuration values.

--- src/NServiceBus.RateLimiter/RateLimiterFeature.cs
+++ src/NServiceBus.RateLimiter/RateLimiterFeature.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using NServiceBus.Features;
+﻿using NServiceBus.Features;
+using NServiceBus.Logging;
 
 class RateLimiterFeature : Feature
 {
     protected override void Setup(FeatureConfigurationContext context)
     {
         if (!context.Settings.TryGet<Properties>(out var properties)) return;
-
+        LogManager.GetLogger("RateLimiter").InfoFormat("Rate limiter configuration: Limit:{0:N0} Duration:{1:g}", properties.Limit, properties.Duration);
         context.Pipeline.Register(behavior: new RateLimitBehavior(properties.Limit, properties.Duration), description: nameof(RateLimitBehavior));
     }
 }

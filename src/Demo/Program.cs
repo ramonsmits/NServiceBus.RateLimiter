2ed1e11937f72e3b6a6bdf5b10a4f9f36bb9a899
Ramon Smits <ramon.smits@gmail.com>
2023-07-16 14:04:02 +0200

    Added demo

---
+++ src/Demo/Program.cs
@@ -0,0 +1,39 @@
+﻿using NServiceBus;
+
+var cfg = new EndpointConfiguration("RateLimiterDemo");
+cfg.UseTransport(new LearningTransport());
+cfg.LimitMessageProcessingConcurrencyTo(100);   // Increase concurrency to the transport will fetch many messages
+
+cfg.ApplyRateLimiting(c =>
+{
+    // One message every 250ms
+    c.Duration = TimeSpan.FromMilliseconds(250);
+    c.Limit = 1;
+    c.StartDurationThreshold = TimeSpan.FromSeconds(1);
+});
+
+var instance = await Endpoint.Start(cfg);
+
+var tasks = new List<Task>();
+for (int i = 0; i < 25; i++)
+{
+    tasks.Add(instance.SendLocal(new MyMessage()));
+}
+await Task.WhenAll(tasks);
+
+Console.ReadKey();
+
+await instance.Stop();
+
+
+class MyMessage : IMessage
+{
+}
+
+class MyHandler : IHandleMessages<MyMessage>
+{
+    public Task Handle(MyMessage message, IMessageHandlerContext context)
+    {
+        return Console.Out.WriteLineAsync("Received a message");
+    }
+}
